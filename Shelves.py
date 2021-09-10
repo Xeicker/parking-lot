56ac94630badc441028019108eafa13d97006e56
Edgar Aguilar <xeicker@gmail.com>
2021-09-10 17:41:49 -0500

    Refactor db access and reinterpret cars as objects
    Delegate db management to ParkingLotManager since it is
    more logical to be there. Implement Car class to better
    managment of car manipulation within the simulated parking
    lot. Enclose db access into a class to be more flexible to
    future changes to this behaviour

--- Shelves.py
+++ Shelves.py
@@ -2,9 +2,31 @@ from flask import g
 import shelve
 
 
-# Read database
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = shelve.open("cars.db")
-    return db
+class Shelves():
+    # Read database, only from file if needed
+    @staticmethod
+    def get_db():
+        db = getattr(g, '_database', None)
+        if db is None:
+            db = g._database = shelve.open("cars.db")
+        return db
+
+
+class Car():
+    def __init__(self, id, tariff, location, start):
+        self.id = id
+        self.tariff = tariff
+        self.location = location
+        self.start = start
+        self.finish = None
+
+    def get_as_dict(self):
+        """Generates a dictionary from car data"""
+        d = {
+            "car": self.id,
+            "tariff": self.tariff,
+            "location": self.location,
+            "start": self.start.strftime("%Y-%m-%d %H:%M:%S")}
+        if self.finish is not None:
+            d["finish"] = self.finish.strftime("%Y-%m-%d %H:%M:%S")
+        return d
