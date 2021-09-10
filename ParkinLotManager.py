9b772a22f84be2a93888ccd7bc84ee4f15b06d46
Edgar Aguilar <xeicker@gmail.com>
2021-09-10 15:59:08 -0500

    Refactor ParkingLotManager module
    Translate prevous code to a class
    Add config file so tariffs are controled from there.

--- ParkinLotManager.py
+++ ParkinLotManager.py
@@ -1,19 +1,38 @@
 import math
-
-tariffs = {"hourly": lambda fromd, tod: basic_tariff(fromd, tod, 3600, 2),
-           "daily": lambda fromd, tod: basic_tariff(fromd, tod, 3600*24, 30)}
-number_of_spots = 10
+import configparser
 
 
-def basic_tariff(fromd, tod, divisor, factor):
-    return math.ceil((tod-fromd).total_seconds()/float(divisor))*factor
+class ParkingLotManager():
+    tariffs = {}
+    parking_spots = -1
 
+    def __init__(self):
+        self.parser = configparser.ConfigParser(allow_no_value=True)
+        self._load_configurations()
 
-def calculate_fee(fromd, tod, tariff):
-    # Any tariff is free first 15 minutes
-    if (tod-fromd).total_seconds() <= 15*60:
-        return 0
+    def _basic_tariff(self, fromd, tod, divisor, factor):
+        return math.ceil((tod-fromd).total_seconds()/float(divisor))*factor
 
-    elif tariff in tariffs:
-        return tariffs[tariff](fromd, tod)
-    raise Exception("Unknown tariff")
+    def _load_configurations(self):
+        self.parser.read("parkinglot.ini")
+        for tariff_type in self.parser["Tariff_Types"]:
+            if tariff_type == "basic_tariffs":
+                for tariff in self.parser[tariff_type]:
+                    tariff_details = \
+                        self.parser[tariff]
+                    self.tariffs[tariff] = lambda fromd, tod:\
+                        self._basic_tariff(fromd,
+                                           tod,
+                                           tariff_details["seconds"],
+                                           tariff_details["cost"])
+        self.parking_spots = \
+            int(self.parser["General_Config"]["parking_spots"])
+
+    def calculate_fee(self, fromd, tod, tariff):
+        # Any tariff is free first 15 minutes
+        if (tod-fromd).total_seconds() <= 15*60:
+            return 0
+
+        elif tariff in self.tariffs:
+            return self.tariffs[tariff](fromd, tod)
+        raise Exception("Unknown tariff")
